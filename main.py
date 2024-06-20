305b3070c3c1ca0abf0bb11e712f4a118286f180
Denis <onishchenko.d@yandex.ru>
2024-06-21 02:06:28 +0300

    Десятый коммит

--- main.py
+++ main.py
@@ -90,9 +90,9 @@ def get_auths_command(update: Update, context):
     ssh_command = "last -n 10"
     return execute_ssh_command(update, context, ssh_command)
 
-def get_critical_command(update: Update, context):
-    ssh_command = "tail -n 5 /var/log/syslog"
-    return execute_ssh_command(update, context, ssh_command,)
+def get_critical_command(update: Update, context, 'Последние 5 критических события'):
+    ssh_command = "journalctl -p crit | tail -n 5"
+    return execute_ssh_command(update, context, ssh_command)
 
 def get_ps_command(update: Update, context):
     ssh_command = "ps"
